beb6f661c64b402f1b9300cd4ededbdd29eef71f
arkaban <arka.purkayastha10@gmail.com>
2022-03-26 14:56:20 +0000

    v1.3 for emmy

--- src/scan.py
+++ src/scan.py
@@ -11,9 +11,9 @@ class scan:
     def __init__(self):
         rospy.init_node('scan_arm',anonymous=True)
         global r
-        r = 1
+        r = 10
         self.rate = rospy.Rate(r)
-        self.joint_sub = rospy.Subscriber("joint_states", JointState, self.scan_callback)
+        self.joint_sub = rospy.Subscriber("joint_states", JointState, self.joint_callback)
         self.joint_pub = rospy.Publisher('joint_trajectory_point', Float64MultiArray, queue_size = 10)    
         self.joint_pos = Float64MultiArray() 
         global pi 
@@ -21,47 +21,61 @@ class scan:
         global n_pi
         n_pi = -3.14
         self.right = True
+        self.time_step = rospy.get_time()
 
-    def scan_callback(self,data):
+    def joint_callback(self,data):
         print("Msg: {}".format(data.header.seq)) 
-        b, s, e, w, g, end = data.position[2], data.position[3], data.position[4], data.position[5], data.position[6], data.position[7]
-        print(f"Joint Positions: \n [Base]: {b} rads | [Shouler]: {s} rads [Elbow]: {e}\n  rad | [Wrist]: {w} rad | [Gripper]: {g}rad\n")
-        #print(f"complete joints data{[data.position]}")
+        b, s, e, w, g, end = round(data.position[2],3), round(data.position[3],3), round(data.position[4],3), round(data.position[5],3), round(data.position[6],3), round(data.position[7],3)
+        print(f"Joint Positions: \n [Base]: {b} \n [Shoulder]: {s} ~ [Elbow]: {e}\n [Wrist]: {w} | [Gripper]: {g}\n ")
+        print(f"complete joints data{[data.position]}")
         print("----------")
+        self.scan_arm(data)
 
+
+    def scan_arm(self,data):
+        #Joint Position vector should contain 6 elements:
+        #[0, shoulder1, shoulder2, elbow, wrist, gripper]
+        b, s, e, w, g = data.position[2], data.position[3], data.position[4], data.position[5], data.position[6]
+        
         #base_angle = 1.0
-        base_angle = self.calculate_base(b)
+        base_angle = self.calculate_base(float(b))
         
         #wrist
         turret_angle = 0.0
         
-        self.joint_pos.data = self.clean_joint_states([base_angle, s, e, w, g, end]) 
+        self.joint_pos.data = self.clean_joint_states([0.0, base_angle, e, w, g, 0]) 
+        print(f"joint_pos cleaned to: {self.joint_pos.data}")
         self.joint_pub.publish(self.joint_pos)
         self.rate.sleep()
 
     def calculate_base(self,x):
-        vel = pi/(10)
+        curr_time = np.array([rospy.get_time()])
+        dt = curr_time - self.time_step
+        self.time_step = curr_time
+
+        vel = pi/r
         print(f"calc_base got {x} and vel is {vel}\n")
         
         if (n_pi<x<pi) and (self.right): #its in bewtween -pi and pi and turning right
             print("its in bewtween -pi and pi and turning right")
-            x =+ vel 
+            x = x + vel 
 
         elif (n_pi<x<pi) and (not self.right): #its in bewtween -pi and pi and turning left
             print("its in bewtween -pi and pi and turning left")
-            x =- vel  
+            x = x - vel  
 
         elif (x>=pi) and (self.right): #need to switch dir to left
             print("need to switch dir to right")
             self.right = False
-            x =- vel
+            x = x - vel
         
         elif (n_pi<=x) and (self.right): #need to switch dir to right
             print("need to switch dir to right")
             self.right = False
-            x =+ vel
+            x = x + vel
+        
         else:
-            x = 0
+            x = 0.0
 
         print(f"new base angle should be: {x}\n")
         return x
